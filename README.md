21a34412ccca7dc01d738d37b941ab1da4075a90
Sunil D S <sunil3590@gmail.com>
2015-03-27 14:04:35 -0400

    v0.1
    Initial checkin

---
+++ README.md
@@ -0,0 +1,2 @@
+# university-housing-app
+Java application for a university housing department. This is an academic project.
---
+++ src/edu/ncsu/sarj/dbproj/UniversityHousingApp.java
@@ -0,0 +1,11 @@
+package edu.ncsu.sarj.dbproj;
+
+public class UniversityHousingApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First version test");
+
+	}
+
+}
