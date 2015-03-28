91e13d1a21da240869ee73a963a4586c5f398722
Sunil D S <sunil3590@gmail.com>
2015-03-27 22:08:27 -0400

    DB connection established
    - Eclipse project files not checked in
    - ojdbc used

---
+++ src/edu/ncsu/sarj/dbproj/DBConnFactory.java
@@ -0,0 +1,74 @@
+package edu.ncsu.sarj.dbproj;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * Deal only with establishing and terminating connection with Oracle10g server
+ * This is done to save from wrapping more methods
+ * 
+ * Singleton design patten
+ * 
+ * All queries and other operations with the SQL connection to be handled outside
+ * this class
+ */
+public class DBConnFactory {
+
+	private static DBConnFactory connection = null;
+	private static Connection sqlConnection = null;
+	
+	private DBConnFactory() {
+		// make it explicit!
+		connection = null;
+		sqlConnection = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			System.err.println("Oracle JDBC driver not loaded " + e);
+		}
+	}
+	
+	public static DBConnFactory getInstance() {
+		if(connection == null) {
+			connection = new DBConnFactory();
+		}
+		
+		return connection;
+	}
+	
+	public Connection getConnection() {
+		if(sqlConnection == null) {
+			try {
+				// NCSU DB server, user name and password
+				String dbUrl = "jdbc:oracle:thin:@ora.csc.ncsu.edu:1521:orcl";
+				String user = "sdavang"; //unityID
+				String pass = "200063506"; //student ID number
+				
+				sqlConnection = (Connection) DriverManager.getConnection(
+						dbUrl, user, pass);
+			} catch (SQLException e) {
+				System.out.println("DBConnFactory.getConnectio() ~ " + e);
+				return null;
+			}
+		}
+		
+		return sqlConnection;
+		
+	}
+	
+	public boolean closeConnection() {
+		if(sqlConnection != null) {
+			try {
+				sqlConnection.close();
+			} catch (SQLException e) {
+				System.out.println("DBConnFactory.closeConnection() ~ " + e);
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+}
--- src/edu/ncsu/sarj/dbproj/UniversityHousingApp.java
+++ src/edu/ncsu/sarj/dbproj/UniversityHousingApp.java
@@ -1,10 +1,36 @@
 package edu.ncsu.sarj.dbproj;
 
+import java.sql.Connection;
+
+/*
+ * Main class
+ */
 public class UniversityHousingApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("First version test");
+		//glorify ourselves
+		System.out.println("*****************************************************");
+		System.out.println("CSC540 - Project 1 - University Housing Application");
+		System.out.println("{sdavang, aravi5, rravi, jlourde} @ncsu.edu");
+		System.out.println("Let's start filling those accomadations!");
+		System.out.println("*****************************************************");
+		
+		//get a connection to the DB before you do anything else
+		Connection conn = DBConnFactory.getInstance().getConnection();
+		if(conn == null) {
+			// nothing to clean up. inform and exit
+			System.out.println("UniversityHousingApp.main() ~ SQL connection error");
+			System.exit(0);
+		}
+		
+		System.out.println("UniversityHousingApp.main() ~ Connected to database");
+		
+		//terminate the DB connection before you quit
+		DBConnFactory.getInstance().closeConnection();
+		
+		System.out.println("UniversityHousingApp.main() ~ Exiting");
+		
+		return;
 
 	}
 
